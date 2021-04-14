1f1c22cf3e0fc248afb33a70a834f2cfef4c2ff8
Max K <knom@users.noreply.github.com>
2021-04-14 17:58:02 +0200

    Updated to latest APIs
    * Updated package.json to fix dependency versions
    * Updated gulpfile
    * Added eslint
    * Moved to Node 10
    * Updated VSS categories
    * Update to Azure-Pipelines-Lib

--- src/swagger-diff/task.js
+++ src/swagger-diff/task.js
@@ -1,69 +1,86 @@
-'use strict';
+"use strict";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var util = require('util');
+var util = require("util");
 // var fail = require("assert").fail;
-var swaggerDiffLib = require('swagger-diff');
-var task = require('vsts-task-lib/task');
+var task = require("azure-pipelines-task-lib/task");
 
-var leftType = task.getInput('leftType', true);
-var rightType = task.getInput('rightType', true);
+var leftType = task.getInput("leftType", true);
+var rightType = task.getInput("rightType", true);
 
 var leftPath = "",
   rightPath = "";
 
 if (leftType === "file") {
-  leftPath = task.getPathInput('leftFilePath', true, true);
+  leftPath = task.getPathInput("leftFilePath", true, true);
 } else {
-  leftPath = task.getInput('leftUrl', true);
+  leftPath = task.getInput("leftUrl", true);
 }
 
 if (rightType === "file") {
-  rightPath = task.getPathInput('rightFilePath', true, true);
+  rightPath = task.getPathInput("rightFilePath", true, true);
 } else {
-  rightPath = task.getInput('rightUrl', true);
+  rightPath = task.getInput("rightUrl", true);
 }
 
 var config = null;
 
-var extConfig = task.getInput('config', false);
+var extConfig = task.getInput("config", false);
+
+var swaggerDiffLib = require("swagger-diff");
 
 if (extConfig && extConfig !== "") {
   config = JSON.parse(extConfig);
 } else {
   config = {
-    "changes": {
-      "breaks": 3,
-      "smooths": 2
-    }
+    changes: {
+      breaks: 3,
+      smooths: 2,
+    },
   };
 }
 
 task.debug("Config: " + JSON.stringify(config));
 
-swaggerDiffLib(leftPath, rightPath, config).then(function (diff) {
-  diff.errors.forEach(function (value) {
-    task.error(util.format("Rule: %s, Message %s", value.ruleId, value.message));
-    task.debug("Error: " + JSON.stringify(value));
-  });
+swaggerDiffLib(leftPath, rightPath, config).then(
+  (diff) => {
+    diff.errors.forEach((value) => {
+      task.error(
+        util.format("Rule: %s, Message %s", value.ruleId, value.message)
+      );
+      task.debug("Error: " + JSON.stringify(value));
+    });
 
-  diff.warnings.forEach(function (value) {
-    task.warning(util.format("Rule: %s, Message %s", value.ruleId, value.message));
-    task.debug("Warning: " + JSON.stringify(value));
-  });
+    diff.warnings.forEach((value) => {
+      task.warning(
+        util.format("Rule: %s, Message %s", value.ruleId, value.message)
+      );
+      task.debug("Warning: " + JSON.stringify(value));
+    });
 
-  diff.infos.forEach(function (value) {
-    console.log(util.format("Rule: %s, Message %s", value.ruleId, value.message));
-    task.debug("Warning: " + JSON.stringify(value));
-  });
+    diff.infos.forEach((value) => {
+      console.log(
+        util.format("Rule: %s, Message %s", value.ruleId, value.message)
+      );
+      task.debug("Warning: " + JSON.stringify(value));
+    });
 
-  // Handle result
-  task.debug(JSON.stringify(diff));
+    // Handle result
+    task.debug(JSON.stringify(diff));
 
-  if (diff.errors && diff.errors.length > 0) {
-    task.setResult(task.TaskResult.Failed, util.format("%s Errors found in Swagger Diff", diff.errors.length));
+    if (diff.errors && diff.errors.length > 0) {
+      task.setResult(
+        task.TaskResult.Failed,
+        util.format("%s Errors found in Swagger Diff", diff.errors.length)
+      );
+    }
+  },
+  (reason) => {
+    task.error(
+      "An error occurred calling swagger-diff" + JSON.stringify(reason)
+    );
+    task.setResult(
+      task.TaskResult.Failed,
+      "An error occurred calling swagger-diff!"
+    );
   }
-
-}, function (reason) {
-  task.error("An error occurred calling swagger-diff" + JSON.stringify(reason));
-  task.setResult(task.TaskResult.Failed, "An error occurred calling swagger-diff!");
-});
+);
